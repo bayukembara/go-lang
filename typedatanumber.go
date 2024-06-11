b963fd2f63fd93515cdc048c51a3da49be78f90f
Bayu K <50491719+bayukembara@users.noreply.github.com>
2024-06-11 23:01:09 +0700

    boolean, need to run go run not just usual compile

---
+++ typedatanumber.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("int8", int8(20))
+	fmt.Println("int16", int16(20000))
+	fmt.Println("int32", int32(20000000))
+	fmt.Println("int64", int64(2000000000000))
+	fmt.Println("uint8", uint8(20))
+	fmt.Println("uint16", uint16(20000))
+	fmt.Println("uint32", uint32(20000000))
+	fmt.Println("uint64", uint64(2000000000000))
+}

05cec2c18c595fe38eb96bcdbfc982c6b7c3ce67
Bayu K <50491719+bayukembara@users.noreply.github.com>
2024-06-11 22:43:24 +0700

    numbers, need to run go run not just usual compile

---
+++ helloworld.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	numbers()
+}
+
+func numbers() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga koma lima = ", 3.5)
+}

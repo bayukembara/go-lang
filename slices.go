5bd645d3ddd97cce25937225a93b7c872cc9face
Bayu K <50491719+bayukembara@users.noreply.github.com>
2024-06-13 07:28:41 +0700

    array, need to run go run not just usual compile

--- slices.go
+++ slices.go
@@ -7,8 +7,12 @@ func main() {
 
 	slice1 := names[2:4]
 	slice2 := names[:4]
+	slice3 := names[1:]
+	slice4 := names[:]
 
 	fmt.Println(names)
 	fmt.Println(slice1)
 	fmt.Println(slice2)
+	fmt.Println(slice3)
+	fmt.Println(slice4)
 }
